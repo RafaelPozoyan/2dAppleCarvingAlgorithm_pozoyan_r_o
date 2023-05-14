1c2330fd172a5484b894e987f25f986b6efd8c5e
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2023-05-14 22:17:55 +0300

    Add files via upload

---
+++ apple_carving/apple_carving/apple_carving.cpp
@@ -0,0 +1,13 @@
+﻿// apple_carving.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+#include "apple_carving.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
