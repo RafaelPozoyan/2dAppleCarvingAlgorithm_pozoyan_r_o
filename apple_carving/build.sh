26ccb68abf5aff7918e8b8615fa08dac5226e980
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2023-05-15 21:20:57 +0300

    Add files via upload

---
+++ apple_carving/build.sh
@@ -0,0 +1,3 @@
+cmake -S . -B build "-DCMAKE_TOOLCHAIN_FILE=/home/rafael/repos/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build ./build --config Release
+# C:/Users/pozoy/vcpkg/scripts/buildsystems/vcpkg.cmake 
---
+++ apple_carving/build/apple_carving/CMakeFiles/apple_carving.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apple_carving.dir/apple_carving.cpp.o"
+  "CMakeFiles/apple_carving.dir/apple_carving.cpp.o.d"
+  "apple_carving"
+  "apple_carving.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apple_carving.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
