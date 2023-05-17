7527f43ee0ee4ef876939377588e08e1ea6fcd98
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2023-05-17 11:19:33 +0300

    Delete apple_carving directory

--- apple_carving/build.sh
+++
@@ -1,3 +0,0 @@
-cmake -S . -B build "-DCMAKE_TOOLCHAIN_FILE=/home/rafael/repos/vcpkg/scripts/buildsystems/vcpkg.cmake"
-cmake --build ./build --config Release
-# C:/Users/pozoy/vcpkg/scripts/buildsystems/vcpkg.cmake 
--- apple_carving/build/apple_carving/CMakeFiles/apple_carving.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for apple_carving.
-# This may be replaced when dependencies are built.
