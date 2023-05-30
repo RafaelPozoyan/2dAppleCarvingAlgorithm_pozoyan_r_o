04403d0d677782d439e3f2a5ded22f5c4f266734
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2023-05-31 00:23:59 +0300

    Update delauney_triangulation.cpp

--- apple_carving/delauney_triangulation.cpp
+++ apple_carving/delauney_triangulation.cpp
@@ -1,193 +1,5 @@
 #include "apple_carving.hpp"
 
-//using namespace cv;
-//using namespace std;
-
-//int main() {
-//    cv::Mat img (600, 700, CV_8UC3, cv::Scalar(0, 0, 0));
-//    std::vector<cv::Point> points1 = {
-//        cv::Point(100, 100),
-//        cv::Point(100, 200),
-//        cv::Point(100, 300),
-//        cv::Point(100, 150),
-//        cv::Point(100, 250),
-//        cv::Point(100, 350),
-//        cv::Point(100, 400),
-//        //
-//
-//        cv::Point(100, 100),
-//        cv::Point(140, 100),
-//        cv::Point(180, 100),
-//        cv::Point(220, 100),
-//        cv::Point(260, 100),
-//        //
-//
-//        cv::Point(100, 200),
-//        cv::Point(130, 200),
-//        cv::Point(160, 200),
-//        cv::Point(190, 200),
-//        cv::Point(210, 200),
-//        //
-//
-//        cv::Point(240, 210),
-//        //
-//
-//        cv::Point(100, 450),
-//        cv::Point(130, 450),
-//        cv::Point(160, 450),
-//        cv::Point(190, 450),
-//        cv::Point(210, 450),
-//        //
-//
-//        cv::Point(240, 440),
-//        //
-//
-//        cv::Point(260, 250),
-//        cv::Point(260, 300),
-//        cv::Point(260, 350),
-//        cv::Point(260, 400)
-//    };
-//    for (const auto& point : points1) {
-//        cv::circle(img, point, 3, cv::Scalar(255, 0, 0), -1);
-//    }
-//
-//
-//
-//    std::vector<cv::Point> points2 = {
-//    cv::Point(350, 450),
-//    cv::Point(370, 420),
-//    cv::Point(390, 390),
-//    cv::Point(410, 360),
-//    cv::Point(430, 330),
-//    cv::Point(450, 300),
-//    cv::Point(470, 270),
-//    cv::Point(490, 240),
-//    cv::Point(510, 210),
-//    cv::Point(530, 180),
-//    cv::Point(550, 150),
-//    cv::Point(570, 120),
-//    cv::Point(580, 100),
-//    //
-//
-//    cv::Point(570, 450),
-//    cv::Point(550, 420),
-//    cv::Point(530, 390),
-//    cv::Point(510, 360),
-//    cv::Point(490, 330),
-//    cv::Point(470, 300),
-//    cv::Point(450, 270),
-//    cv::Point(430, 240),
-//    cv::Point(410, 210),
-//    cv::Point(390, 180),
-//    cv::Point(370, 150),
-//    cv::Point(350, 120),
-//    cv::Point(340, 100),
-//    //
-//
-//    cv::Point(460, 285),
-//    cv::Point(460, 235),
-//    cv::Point(460, 185),
-//    cv::Point(460, 135),
-//    cv::Point(460, 100),
-//    cv::Point(460, 315),
-//    cv::Point(460, 365),
-//    cv::Point(460, 415),
-//    cv::Point(460, 450),
-//    };
-//    for (const auto& point : points2) {
-//        cv::circle(img, point, 3, cv::Scalar(255, 0, 0), -1);
-//    }
-//
-//
-// //////////////////////////////////////////////////////////////////////////////////////
-//
-//
-//    cv::Point p1(100, 100);
-//    cv::Point p2(100, 450);
-//
-//    cv::line(img, p1, p2, cv::Scalar(0, 0, 255), 2);
-//
-//    
-//    cv::Point p3(100, 100);
-//    cv::Point p4(260, 100);
-//
-//    cv::line(img, p3, p4, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p5(100, 200);
-//    cv::Point p6(210, 200);
-//
-//    cv::line(img, p5, p6, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p7(100, 450);
-//    cv::Point p8(210, 450);
-//
-//    cv::line(img, p7, p8, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p9(260, 250);
-//    cv::Point p10(260, 400);
-//
-//    cv::line(img, p9, p10, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p11(210, 200);
-//    cv::Point p12(240, 210);
-//
-//    cv::line(img, p11, p12, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p13(240, 210);
-//    cv::Point p14(260, 250);
-//
-//    cv::line(img, p13, p14, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p15(210, 450);
-//    cv::Point p16(240, 440);
-//
-//    cv::line(img, p15, p16, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p17(240, 440);
-//    cv::Point p18(260, 400);
-//
-//    cv::line(img, p17, p18, cv::Scalar(0, 0, 255), 2);
-//
-//
-//
-//    cv::Point p19(350, 450);
-//    cv::Point p20(570, 120);
-//
-//    cv::line(img, p19, p20, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p21(580, 100);
-//
-//    cv::line(img, p20, p21, cv::Scalar(0, 0, 255), 2);
-//
-//
-//    cv::Point p22(570, 450);
-//    cv::Point p23(350, 120);
-//
-//    cv::line(img, p22, p23, cv::Scalar(0, 0, 255), 2);
-//
-//    cv::Point p24(340, 100);
-//
-//    cv::line(img, p23, p24, cv::Scalar(0, 0, 255), 2);
-//
-//    cv::Point p25(460, 450);
-//    cv::Point p26(460, 100);
-//
-//    cv::line(img, p25, p26, cv::Scalar(0, 0, 255), 2);
-//
-//    cv::imshow("bulka'", img);
-//    cv::waitKey(0);
-//    
-//    return 0;
-//}
-
 
 
 int main(){
@@ -199,20 +11,45 @@ int main(){
     auto x = 1000; // размер окна
     auto y = 1000; // размер окна
     int num = 100; // количество точек
-    int point_size = 3;
+    int point_size = 1;
 
     ///////////////////////////////////////////////////////////
 
-    // Generate num random points
-    std::vector<cv::Point2f> points; // point2f - тип для точек в 2д
-    cv::RNG rng; // 
-    for (int i = 0; i < num; i++) {
-        points.push_back(cv::Point2f(rng.uniform(0, x+50), rng.uniform(0, y+25))); // в каком диапозоне подбираются точки
+     //Generate num random points
+    std::vector<int> points_one_len; // point2f - тип для точек в 2д
+
+
+    std::string str;
+    //в тестах приведены числа в диапазоне от 100 до 1000
+ //   std::ifstream file("C:/Users/pozoy/Desktop/TestFiles/test_3(100).txt"); // 100 чисел
+    std::ifstream file("D:/apple_carving_pozoyan_r_o-master/apple_carving_pozoyan_r_o-master/apple_carving/test/test_2(500).txt");  // 500 чисел
+
+    if (!file.is_open()) {
+        std::cout << "The file can't be opened!\n";
+    }
+    else {
+        while (file) {
+            getline(file, str);
+            int int_number_translate = atoi(str.c_str());
+            points_one_len.push_back(int_number_translate);
+        }
     }
 
+    file.close();
+
+    std::vector<cv::Point2i> points;
+    for (int i = 0; i < points_one_len.size(); i += 2) {
+        cv::Point2i new_point(points_one_len[i], points_one_len[i+1]);
+        points.push_back(new_point);
+    }
+
+    //for (int t = 0; t < points.size(); t++) {
+    //    std::cout << points[t];
+    //}
 
 
-    // Perform Delaunay triangulation
+
+     ////Perform Delaunay triangulation
     cv::Subdiv2D subdiv(cv::Rect(0, 0, point_del, point_del));
     for (const auto& p : points) {
         subdiv.insert(p);
@@ -220,7 +57,7 @@ int main(){
 
 
 
-    // Draw Delaunay triangles
+     ////Draw Delaunay triangles
     cv::Mat img(y + 100, x + 100, CV_8UC3, cv::Scalar(0, 0, 0));
     std::vector<cv::Vec6f> triangles;
     subdiv.getTriangleList(triangles);
@@ -238,7 +75,7 @@ int main(){
             }
         }
         if (cnt == 6) {
-            /*std::cout << "Triangle: (" << t[0] << ", " << t[1] << "), (" << t[2] << ", " << t[3] << "), (" << t[4] << ", " << t[5] << ")" << std::endl;*/
+            //std::cout << "Triangle: (" << t[0] << ", " << t[1] << "), (" << t[2] << ", " << t[3] << "), (" << t[4] << ", " << t[5] << ")" << std::endl;
             for (int l = 0; l < 6; l++) {
                 list.push_back(t[l]);
             }
@@ -247,9 +84,9 @@ int main(){
 
 
 
-    for (int i = 0; i < list.size(); i++) {
-        std::cout << list[i] << std::endl;
-    }
+    //for (int i = 0; i < list.size(); i++) {
+    //    std::cout << list[i] << std::endl;
+    //}
 
 
 
@@ -270,14 +107,24 @@ int main(){
                 cv::circle(img, p, point_size, cv::Scalar(0, 0, 255), -1);
             }
             cv::imshow("Delaunay Triangulation", img);
-            cv::waitKey(150);
+            if (num > 100) {
+                cv::waitKey(25);
+            }
+            else {
+                cv::waitKey(75);
+            }
         }
     }
+
+
 
     // Display image
     //cv::imshow("Delaunay Triangulation", img);
     /*cv::waitKey(0);*/
 }
+
+
+
 //
 //#include <opencv2/core.hpp>
 //#include <opencv2/imgproc.hpp>
