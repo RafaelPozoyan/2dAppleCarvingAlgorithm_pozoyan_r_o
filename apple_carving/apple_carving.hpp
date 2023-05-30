36c0ebb862b1f31bd315176ac2410d7cda249c77
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2023-05-31 00:24:23 +0300

    Update apple_carving.hpp

--- apple_carving/apple_carving.hpp
+++ apple_carving/apple_carving.hpp
@@ -1,4 +1,4 @@
-﻿// apple_carving.h : включаемый файл для стандартных системных включаемых файлов
+// apple_carving.hpp : включаемый файл для стандартных системных включаемых файлов
 // или включаемые файлы для конкретного проекта.
 
 #pragma once
@@ -6,4 +6,43 @@
 #include <iostream>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <sstream>
+#include <fstream>
+#include <string>
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+
+//class Apple_Carving{
+//public:
+//	
+//	Apple_Carving() = default;
+//	
+//
+//	~Apple_Carving() = default;
+//	
+//
+//	Apple_Carving(const Apple_Carving&) = default;
+//
+//
+//	void delauney_triangle(int point_del, int x, int y, int num, int point_size/*, cv::Mat& img*/);
+//
+//
+//	Apple_Carving(int point_dell, int xx, int yy, int numm, int point_sizee)
+//		: point_del(point_dell), x(xx), y(yy), num(numm), point_size(point_sizee)
+//	{
+//	}
+//
+//
+//	bool operator==(const Apple_Carving& other) const{
+//		return false;
+//	}
+//
+//
+//	std::ostream& operator<<(std::ostream& ostrm/*, const cv::Mat& img*/) ; // return o.writeTo(ostrm);
+//
+//private:
+//	int point_del = 10000; //для удаления прямых
+//	int x = 1000; // размер окна
+//	int y = 1000; // размер окна
+//	int num = 10; // количество точек
+//	int point_size = 300 / num;
+//};
